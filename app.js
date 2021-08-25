3453ad050ced59a2f8bd3e2a12ed0313cf3f213e
Jordan Kimsey <jordankimsey@gmail.com>
2021-08-25 13:11:08 -0400

    Added reset functionally to button

--- app.js
+++ app.js
@@ -21,8 +21,8 @@ function amountPerPerson(total, percent, ppl) {
     let tipToPay = tips / ppl;
     let totalMoneyPerPerson = total / ppl + tipToPay;
 
-    tipPerPerson.innerHTML = tipToPay.toFixed(2);
-    totalPerPerson.innerHTML = totalMoneyPerPerson.toFixed(2);
+    tipPerPerson.innerHTML = `$ ${tipToPay.toFixed(2)}`;
+    totalPerPerson.innerHTML = `$ ${totalMoneyPerPerson.toFixed(2)}`;
 
     //add reset 
 }
@@ -40,9 +40,18 @@ function people(value) {
 
 function currentTip(value) {
     currentTipValue = value;
+
+    tipPercent.forEach(tip => {
+        document.querySelectorAll('.selected').forEach(select => {
+            select.classList.remove('selected');
+        })
+        tip.classList.add('selected');
+    })
+
+
+
     amountPerPerson(currentBillAmount, currentTipValue, currentNumberOfPeople);
 }
-
 
 //Select Tip amount and add selected class
 tipPercent.forEach(tip => {
@@ -57,9 +66,27 @@ tipPercent.forEach(tip => {
         currentTipValue = e.target.value;
         amountPerPerson(currentBillAmount, currentTipValue, currentNumberOfPeople);
         //need to update global current tip value
-        // currentTipValue = tip.value;
+         currentTipValue = tip.value;
     })
 })
+
+reset.addEventListener('click', (e) => {
+    currentBillAmount = '';
+    currentNumberOfPeople = '';
+    currentTipValue = '';
+    billAmount.value = '';
+    numberOfPeople.value = '';
+
+    tipPerPerson.innerHTML = `$0.00`;
+    totalPerPerson.innerHTML = `$0.00`;
+
+    tipPercent.forEach(tip => {
+        document.querySelectorAll('.selected').forEach(select => {
+            select.classList.remove('selected');
+        })
+    })
+})
+
 
 
 
